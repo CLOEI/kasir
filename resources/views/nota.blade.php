1035522c47bbf06c83c4d0ae2833cb7d1a1536cb
Cendy <cendyemail@gmail.com>
2024-08-20 15:47:44 +0700

    feat: calculate tax

--- resources/views/nota.blade.php
+++ resources/views/nota.blade.php
@@ -1,6 +1,13 @@
 @extends('layout')
 
 @section('content')
+    @php
+        $total = 0;
+        foreach ($transaksi->transactionDetails as $item) {
+            $total += $item->quantity * $item->product->price;
+        }
+        $totalIncludingTax = $total;
+    @endphp
     <div class="bg-gray-600 min-h-screen p-4">
         <div class="bg-white p-4 rounded-md max-w-sm mx-auto text-center">
             <h1 class="font-semibold text-3xl">B0{{ $transaksi->id }}</h1>
@@ -23,18 +30,18 @@
                     <div>
                         <p>{{ $item->product->name }}</p>
                         <div class="grid grid-cols-2">
-                            <p>1 x Rp. {{ number_format($item->product->price) }}</p>
-                            <p>: Rp. {{ number_format($item->product->price) }}</p>
+                            <p>{{ $item->quantity }} x Rp. {{ number_format($item->product->price) }}</p>
+                            <p>: Rp. {{ number_format($item->product->price * $item->quantity) }}</p>
                         </div>
                         @foreach ($item->product->productItems as $productItem)
-                            <p class="ml-2">- 1 x {{ $productItem->name }}</p>
+                            <p class="ml-2">- {{ $item->quantity }} x {{ $productItem->name }}</p>
                         @endforeach
                     </div>
                     <div class="border-b-4 border-dotted my-4"></div>
                 @endforeach
             </div>
             <div class="text-right">
-                <p>TOTAL (Termasuk Pajak) : Rp. {{ number_format($transaksi->total) }}</p>
+                <p>TOTAL (Termasuk Pajak) : Rp. {{ number_format($totalIncludingTax) }}</p>
                 <p>JUMLAH BAYAR : Rp. {{ number_format($transaksi->bayar) }}</p>
                 <p>KEMBALIAN : Rp. {{ number_format($transaksi->kembali) }}</p>
                 <p>MTD BAYAR : Tunai</p>
@@ -42,9 +49,9 @@
             </div>
             <div class="mt-8 grid grid-cols-2">
                 <p>NET SALES</p>
-                <p>: Rp. {{ number_format($transaksi->total) }}</p>
-                <p>PBI</p>
-                <p>: Rp. 0</p>
+                <p>: Rp. {{ number_format($totalIncludingTax - $totalIncludingTax * 0.1) }}</p>
+                <p>PB1</p>
+                <p>: Rp. {{ number_format($totalIncludingTax * 0.0909) }}</p>
             </div>
             <div class="flex items-center justify-center my-4">
                 <div class="flex-grow border-b-4 border-dotted"></div>
