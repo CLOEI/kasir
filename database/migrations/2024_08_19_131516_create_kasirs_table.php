5aa7f33ed131b09843b9b52597688b0fa9d4c8a8
Cendy <cendyemail@gmail.com>
2024-08-21 11:53:47 +0700

    fix: name is not unique

--- database/migrations/2024_08_19_131516_create_kasirs_table.php
+++ database/migrations/2024_08_19_131516_create_kasirs_table.php
@@ -11,8 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('cashiers', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 255)->collation('utf8mb4_unicode_ci');
+            $table->string('name')->unique();
         });
     }
 
